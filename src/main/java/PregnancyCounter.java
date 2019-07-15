201991dd1bc350165383e6a93e89abee59660aa2
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-07-15 15:37:56 +0200

    utworzenie klasy PregnancyCounter

---
+++ src/main/java/PregnancyCounter.java
@@ -0,0 +1,6 @@
+public class PregnancyCounter {
+    public static int pregnancyCount(double yearsOlder, double yearsTo, double timesYounger){
+        return (int) Math.abs(12*((yearsOlder+yearsTo)-(timesYounger*yearsTo))/(timesYounger-1));
+
+    }
+}
